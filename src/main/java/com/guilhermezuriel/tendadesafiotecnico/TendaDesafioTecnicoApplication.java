bbcf9904219111d2767995b7e5209e9658d0cf81
Guilherme Zuriel <guizuriel.dev@gmail.com>
2026-04-04 20:50:06 -0300

    feat: initial commit

---
+++ src/main/java/com/guilhermezuriel/tendadesafiotecnico/TendaDesafioTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermezuriel.tendadesafiotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TendaDesafioTecnicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TendaDesafioTecnicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guilhermezuriel/tendadesafiotecnico/TendaDesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermezuriel.tendadesafiotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TendaDesafioTecnicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
