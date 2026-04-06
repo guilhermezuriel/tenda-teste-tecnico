3a0b7b707589d0ca3e70d42efbb77c702b3f66d0
Guilherme Zuriel <guizuriel.dev@gmail.com>
2026-04-05 23:48:41 -0300

    fix: ajuste na migration para se adequar a corretas tipagens

--- src/main/resources/db/migration/V1__create_coupons_table.sql
+++ src/main/resources/db/migration/V1__create_coupons_table.sql
@@ -3,10 +3,9 @@ CREATE TABLE coupons (
                          code VARCHAR(20) NOT NULL,
                          description TEXT NOT NULL,
                          discount_value DECIMAL(10,2) NOT NULL,
-                         expiration_date DATE NOT NULL,
+                         expiration_date TIMESTAMP NOT NULL,
                          status VARCHAR(20) NOT NULL,
                          published BOOLEAN NOT NULL DEFAULT FALSE,
                          redeemed BOOLEAN NOT NULL DEFAULT FALSE,
-                         created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-                         deleted_at TIMESTAMP NULL
+                         created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
