3109d16e783f0f0d94202251865a0707cb464853
Guilherme Zuriel <guizuriel.dev@gmail.com>
2026-04-05 23:17:24 -0300

    feat: criacao de entidade de banco de dados

---
+++ src/main/java/com/guilhermezuriel/tendadesafiotecnico/infrastructure/persistence/entities/CouponJpaEntity.java
@@ -0,0 +1,51 @@
+package com.guilhermezuriel.tendadesafiotecnico.infrastructure.persistence.entities;
+
+
+import com.guilhermezuriel.tendadesafiotecnico.domain.entities.enums.CouponStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "coupons")
+@Getter
+@Setter
+@NoArgsConstructor
+public class CouponJpaEntity {
+
+    @Id
+    private String id;
+
+    @Column( length = 6, nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "discount_value", nullable = false)
+    private BigDecimal discountValue;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDateTime expirationDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CouponStatus status;
+
+    @Column(nullable = false)
+    private boolean published;
+
+    @Column(nullable = false)
+    private boolean redeemed;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
