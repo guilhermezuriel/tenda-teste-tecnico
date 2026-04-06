226d0f26acb6e8e9796e44cd5ff5a38f08fd9ea8
Guilherme Zuriel <guizuriel.dev@gmail.com>
2026-04-06 00:10:14 -0300

    config: classe de configuracao do swagger

---
+++ src/main/java/com/guilhermezuriel/tendadesafiotecnico/interfaces/http/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.guilhermezuriel.tendadesafiotecnico.interfaces.http.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tenda - Desafio Técnico")
+                        .description("API de gerenciamento de cupons de desconto")
+                        .version("1.0.0")
+                );
+    }
+}
